c6d17327a402dc1a0d937235c0ef9c63f4dcb087
omid <o.shali01@gmail.com>
2024-02-17 12:17:54 +0330

    Edit something

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import Todo from "./components/Todo";
 function App() {
   const [todos, setTodos] = useState(["do homeWork", "washe dishes"]);
   return (
-    <div className="max-w-[1640px] h-screen bg-gradient-to-r from-[#D9AFD9] to-[#97D9E1] p-4">
-      <div className=" mx-auto h-full bg-gray-100/70 border rounded-lg p-4 shadow-lg ">
+    <div className=" h-screen bg-gradient-to-r from-[#D9AFD9] to-[#97D9E1] p-4">
+      <div className="max-w-lg mx-auto h-full bg-gray-100/70 border rounded-lg p-4 shadow-lg ">
         <h1 className="text-center font-bold text-3xl">Todo App</h1>
         <form className="w-full flex justify-between gap-2 mt-4">
-          <input className="w-full p-2" placeholder="Add todos" type="text" />
-          <button className="bg-purple-500 text-white px-4 "><FaPlus size={15}/></button>
+          <input
+            className="w-full p-2 text-xl"
+            placeholder="Add todos"
+            type="text"
+          />
+          <button className="bg-purple-500 text-white px-4 ">
+            <FaPlus size={15} />
+          </button>
         </form>
         <ul>
           {todos.map((todo, inex) => (
